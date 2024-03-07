62f56d39bca6a4bf656ab24a620642d8d4951160
Arthur Duvillaret <aduvilla@bess-f4r3s14.clusters.42paris.fr>
2024-03-07 16:47:38 +0100

    gestion des arguments

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: aduvilla <aduvilla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 15:17:00 by aduvilla          #+#    #+#             */
-/*   Updated: 2024/03/07 15:27:07 by aduvilla         ###   ########.fr       */
+/*   Updated: 2024/03/07 16:47:08 by aduvilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,9 @@ int	main(int ac, char **av, char **env)
 	char	*read;
 
 	(void)av;
-	(void)ac;
 	(void)env;
+	if (ac > 1)
+		return (ft_putstr_fd("Error\nminishell take no argument!\n", 2), 1);
 	while (1)
 	{
 		read = readline("minishell > ");
