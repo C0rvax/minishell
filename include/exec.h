70f1d2e2f45bc0f0bb95755739ba1b8d8e527b8b
ctruchot <ctruchot@student.42.fr>
2024-03-21 16:13:19 +0100

    h aligned

--- include/exec.h
+++ include/exec.h
@@ -6,7 +6,7 @@
 /*   By: ctruchot <ctruchot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 10:55:45 by ctruchot          #+#    #+#             */
-/*   Updated: 2024/03/21 16:10:56 by ctruchot         ###   ########.fr       */
+/*   Updated: 2024/03/21 16:12:57 by ctruchot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,31 +35,31 @@ typedef struct s_child
 
 // exec.c
 
-int	exec(t_cmd *cmd, t_persistent *pers);
-int initialize_exec(t_exec *exec, t_cmd *cmd, char **mini_env);
-int	exec_uno(t_exec *exec);
-int	manage_fds(t_cmd *cmd);
+int			exec(t_cmd *cmd, t_persistent *pers);
+int			initialize_exec(t_exec *exec, t_cmd *cmd, char **mini_env);
+int			exec_uno(t_exec *exec);
+int			manage_fds(t_cmd *cmd);
 
 // exec2.c
-int		create_pipes(t_exec *exec, int total_cmd);
-int		ft_fork(t_exec *exec);
-int		initialize_child(t_child *child, t_exec *exec);
-int		redirect_pipes(t_exec *exec, t_child *child);
-void	close_higher_fds(t_exec *exec);
-int manage_fd_firstchild(t_exec *exec, t_child *child);
-int manage_fd_middlechild(t_exec *exec, t_child *child);
-int manage_fd_lastchild(t_exec *exec, t_child *child);
-void close_all_fds(t_exec *exec);
-int		manage_fd_firstchild(t_exec *exec, t_child *child);
-int		manage_fd_middlechild(t_exec *exec, t_child *child);
-int		manage_fd_lastchild(t_exec *exec, t_child *child);
+int			create_pipes(t_exec *exec, int total_cmd);
+int			ft_fork(t_exec *exec);
+int			initialize_child(t_child *child, t_exec *exec);
+int			redirect_pipes(t_exec *exec, t_child *child);
+void		close_higher_fds(t_exec *exec);
+int			manage_fd_firstchild(t_exec *exec, t_child *child);
+int			manage_fd_middlechild(t_exec *exec, t_child *child);
+int			manage_fd_lastchild(t_exec *exec, t_child *child);
+void		close_all_fds(t_exec *exec);
+int			manage_fd_firstchild(t_exec *exec, t_child *child);
+int			manage_fd_middlechild(t_exec *exec, t_child *child);
+int			manage_fd_lastchild(t_exec *exec, t_child *child);
 
 //  clean.c
-void	clean_exit_parent(t_exec *exec, int err);
-int		clean_end(t_exec *exec);
-int		clean_exit_fds(t_exec *exec, t_child *child);
-int		free_tab_int(int **fd, int nb);
-void	clean_exit_child(t_exec *exec, int err); // clean exit exec ?
-void	close_all_fds(t_exec *exec);
+void		clean_exit_parent(t_exec *exec, int err);
+int			clean_end(t_exec *exec);
+int			clean_exit_fds(t_exec *exec, t_child *child);
+int			free_tab_int(int **fd, int nb);
+void		clean_exit_child(t_exec *exec, int err);
+void		close_all_fds(t_exec *exec);
 
 #endif
